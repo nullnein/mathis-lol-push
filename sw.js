791a916fbb5614f4925bf963f71318dd39517620
nullnein <mathis@oija.de>
2024-02-02 18:23:53 +0100

    had to reset whole local setup, tag dw problem. now updated with hydrogen piece

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1701367138';
+const cacheName = 'chirpy-1706894589';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
