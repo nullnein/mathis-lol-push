1950dcb41acf5f681ecc0d2779b97162ddee66dc
nullnein <mathis@oija.de>
2024-05-04 17:37:21 +0200

    Added two work samples, workeer, ammortization account

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707674253';
+const cacheName = 'chirpy-1714836992';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
