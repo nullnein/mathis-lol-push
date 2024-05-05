3202dab63f88afa09794672a91f4f7e6426a7e47
nullnein <mathis@oija.de>
2024-05-05 03:40:57 +0200

    hopefully fixed pictures issue: capitalization on .img

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1714836992';
+const cacheName = 'chirpy-1714873196';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
