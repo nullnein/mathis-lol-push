ac328e55ac18dd06477d0e2d1ceb1d70352b381a
nullnein <mathis@oija.de>
2024-11-04 18:51:06 +0100

    typo in latest

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1730678258';
+const cacheName = 'chirpy-1730742629';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
