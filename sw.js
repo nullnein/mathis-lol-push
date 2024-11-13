c264bbce1e2a76a14691457a1fcc4d1979c61029
nullnein <mathis@oija.de>
2024-11-13 23:30:40 +0100

    added schuldenbremse post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1730742629';
+const cacheName = 'chirpy-1731537015';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
