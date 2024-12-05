c721770ce25f38b74f66c24294ad801d7ef3bbcf
nullnein <mathis@oija.de>
2024-12-05 10:57:55 +0100

    added newest piece VW, small edits previous posts

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1731537015';
+const cacheName = 'chirpy-1733392544';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
