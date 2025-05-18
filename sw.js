78b72134c3c0a4b73608a24e480d6ce3af1a590c
nullnein <mathis@oija.de>
2025-05-18 16:15:34 +0200

    Added Mar-a-lago story to portfolio

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1742395202';
+const cacheName = 'chirpy-1747577663';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
