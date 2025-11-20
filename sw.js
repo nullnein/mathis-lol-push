b6b39fb9237a7285a2b80c468aa4f723236a8498
nullnein <mathis@oija.de>
2025-11-20 20:20:37 +0100

    updated geoecon 1

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1763261808';
+const cacheName = 'chirpy-1763666394';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
